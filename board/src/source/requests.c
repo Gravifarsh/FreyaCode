2f5549fcd4d6a5766a8703056e30dc9c1b4b9c34
Inversion Spaces <SvetodiodGaming@yandex.ru>
2018-06-10 14:38:52 +0300

    Updated requests

--- board/src/source/requests.c
+++ board/src/source/requests.c
@@ -9,48 +9,38 @@
 #include "geiger.h"
 #include "mq7.h"
 
-void gps_request(){
-	float lon, lat, h;
-	bool hasFix;
-
-	rscs_gps_read(gps, &lon, &lat, &h, &hasFix);
+void gps_request(float* lon, float* lat, float* h, bool* hasFix){
+	rscs_gps_read(gps, lon, lat, h, hasFix);
 }
 
-void cdm_request(){
-	uint16_t conc; //TODO
-
-	rscs_cdm7160_read(cdm, &conc);
+void cdm_request(uint16_t* conc){
+	rscs_cdm7160_read(cdm, conc);
 }
 
-void bmp_request(){
+void bmp_request(int32_t* bpress, int32_t* btemp){
 	spi_set(BMP);
 
-	int32_t rbpress, rbtemp, bpress, btemp; //TODO
+	int32_t rbpress, rbtemp;
 
 	rscs_bmp280_read(bmp, &rbpress, &rbtemp);
-	rscs_bmp280_calculate(rscs_bmp280_get_calibration_values(bmp), rbpress, rbtemp, &bpress, &btemp);
+	rscs_bmp280_calculate(rscs_bmp280_get_calibration_values(bmp), rbpress, rbtemp, bpress, btemp);
 }
 
-void ds_request(){
-	int16_t dtemp; //TODO
-
+void ds_request(int16_t* dtemp){
 	if(rscs_ds18b20_check_ready()){
-		rscs_ds18b20_read_temperature(ds, &dtemp);
+		rscs_ds18b20_read_temperature(ds, dtemp);
 		rscs_ds18b20_start_conversion(ds);
 	}
 }
 
-void mq7_request(){
-	uint16_t mconc; //TODO
-	mconc = sides_mq7_get();
+void mq7_request(uint16_t* mconc){
+	*mconc = sides_mq7_get();
 }
 
-void geiger_request(){
-	uint32_t ticks; //TODO
-	ticks = sides_gegiger_get_ticks();
+void geiger_request(uint32_t* ticks){
+	*ticks = sides_gegiger_get_ticks();
 }
 
-void time_request(){
-	uint32_t time; //TODO
-	time = rscs_time_get();
+void time_request(uint32_t* time){
+	*time = rscs_time_get();
 }
--- board/src/telemtry.h
+++ board/src/telemtry.h
@@ -8,18 +8,6 @@
 #ifndef PACKET_H_
 #define PACKET_H_
 
-#pragma pack(push, 1)
-typedef struct{
-	int32_t time;
-	int32_t bpress, btemp;
-	int16_t x, y, z;
-	int16_t dtemp;
-	uint16_t conc;
-	uint16_t mconc;
-	float lon, lat;
-}packet_t;
-#pragma pack(pop)
-
 void sd_telemetry_drop();
 void iridium_telemetry_drop();
 void nrf_telemetry_drop();
